0b69df66d7fcee51b11a6409671260a025221b06
Amritasha Agrawal <amritasha71@gmail.com>
2017-03-10 19:59:39 +0530

    url parse

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,8 +1,18 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+
+
+
 /**
  * Created by amritasha on 10/03/17.
  */
 public class Scraper {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        final Document document = Jsoup.connect("http://www.imdb.com/chart/top").get();
+        System.out.println(document.outerHtml());
+
 
     }
 }
